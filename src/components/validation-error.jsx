9b622311f3cf8973e37c350fb920a0a0443f34c6
Avazbek Tojimamatov <tojimamatov.avazbek2002@gmail.com>
2023-03-17 00:23:18 +0500

    Localstorage and Token

--- src/components/validation-error.jsx
+++ src/components/validation-error.jsx
@@ -3,7 +3,6 @@ import {useSelector} from 'react-redux'
 
 const ValidationError = () => {
     const {error} = useSelector(state => state.auth);
-
     const errorMessage = useCallback(() => {
         return Object.keys(error).map(name => {
             const msg = error[name].join(', ');
